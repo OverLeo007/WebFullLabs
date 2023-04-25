ca3eb61d47543638c55edeb7b0c17d1d134e3a6b
OverLeo007 <sokolov_lev2003@mail.ru>
2023-04-25 19:16:13 +0700

    Lab5 added

---
+++ Lab5/static/js/alert.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+    const alert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
+
+    const alertTrigger = document.getElementById('sadAlertBtn')
+    if (alertTrigger) {
+        alertTrigger.addEventListener('click', () => {
+            alert('Ну и пожалуйста, не очень то и хотелось', 'danger')
+        })
+    }
+}
